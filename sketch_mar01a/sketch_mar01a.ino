4d86960a84dec93bbb0a7f0864322e8424e3eec7
Saugat Tripathi <saugattripathi86@gmail.com>
2021-03-01 14:13:45 +0545

    Arduino project

---
+++ sketch_mar01a/sketch_mar01a.ino
@@ -0,0 +1,40 @@
+
+#include <SoftwareSerial.h>
+
+// Configure software serial port
+SoftwareSerial SIM900(7, 8); 
+
+void setup() {
+  // Arduino communicates with SIM900 GSM shield at a baud rate of 19200
+  // Make sure that corresponds to the baud rate of your module
+  SIM900.begin(19200);
+  // Give time to your GSM shield log on to network
+  delay(20000);   
+  
+  // Send the SMS
+  sendSMS();
+}
+
+void loop() { 
+  
+}
+
+void sendSMS() {
+  // AT command to set SIM900 to SMS mode
+  SIM900.print("AT+CMGF=1\r"); 
+  delay(100);
+
+  SIM900.println("AT+CMGS=\"+9779817233070\""); 
+  delay(100);
+  
+  // REPLACE WITH YOUR OWN SMS MESSAGE CONTENT
+  SIM900.println("Nabin is chutiya."); 
+  delay(100);
+
+  // End AT command with a ^Z, ASCII code 26
+  SIM900.println((char)26); 
+  delay(100);
+  SIM900.println();
+  // Give module time to send SMS
+  delay(5000); 
+}
